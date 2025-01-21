594c5a36f67c871109c71fb2456481b8ce0a09cc
ITBukitMega <bryan.jonathan@bukitmega.com>
2025-01-21 16:09:23 +0700

    memindahkan semuanya ke route api untuk dicoba di android tanpa csrf without middleware biar lebih aman

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\LoginControllerProduction;
 
 Route::post("/login", [LoginControllerProduction::class, 'login']);
+Route::post('/attendance/check-in', [AttendanceController::class, 'checkIn']);
+Route::post('/attendance/check-out', [AttendanceController::class, 'checkOut']);
+Route::get('api/attendance/status', [AttendanceController::class, 'checkStatus']);
