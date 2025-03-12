ca03c79a1f730b6090bfb79494389d648758af91
ITBukitMega <bryan.jonathan@bukitmega.com>
2025-03-12 16:10:29 +0700

    menambahkan EmpName di api agar bisa diakses di android

--- app/Http/Controllers/LoginControllerProduction.php
+++ app/Http/Controllers/LoginControllerProduction.php
@@ -65,6 +65,7 @@ class LoginControllerProduction extends Controller
             'message' => 'Login Successful, Welcome Back',
             'data' => [
                 'EmpID' => $user->EmpID,
+                'EmpName' => $user->EmpName,
                 'SiteName' => $user->SiteName,
                 'Shift' => $user->Shift,
                 'Used' => $user->Used, // Added Used field to response
