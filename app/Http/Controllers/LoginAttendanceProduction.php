46d3b965a437482ae943fbd24387174254ed62d3
ITBukitMega <bryan.jonathan@bukitmega.com>
2025-03-26 11:09:12 +0700

    coba naikin versi minimum NEAT jadi 1.1.1 apakah work biometricnya jika version sudah naik

--- app/Http/Controllers/LoginAttendanceProduction.php
+++ app/Http/Controllers/LoginAttendanceProduction.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Validator;
 
 class LoginAttendanceProduction extends Controller
 {
-    private $MINIMUM_APP_VERSION = "1.0.0";
+    private $MINIMUM_APP_VERSION = "1.1.1";
 
     public function login(Request $request)
     {
